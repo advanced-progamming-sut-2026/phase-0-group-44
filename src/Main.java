32addf6fb6d02138824974b459f356d3d46394c1
Yourgirlseta <set.forouzan.87@sharif.edu>
2026-08-02 11:55:16 +0330

    plantSelection capacity reduced to 5 and persian supported

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import controller.App;
 import model.Store;
 import view.MenuRouter;
 
+import java.io.UnsupportedEncodingException;
 import java.util.Scanner;
 
 public final class Main {
@@ -9,12 +10,14 @@ public final class Main {
     private Main() {
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedEncodingException {
         App app = new App();
         System.out.println(app.start().getMessage());
 
         MenuRouter router = new MenuRouter(app);
         Scanner scanner = new Scanner(System.in);
+
+        System.setOut(new java.io.PrintStream(System.out, true, "UTF-8"));
 
         while (Store.isRunning() && scanner.hasNextLine()) {
             router.route(scanner.nextLine());
